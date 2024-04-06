49066a14c915301577fe44e4a5486f1795562706
kermattC <matthew.chan5@uoit.net>
2024-04-05 21:37:36 -0400

    Updated server.py. Preivously updated wrong file on accident

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,14 +1,19 @@
+import os
 from flask import Flask, json, jsonify
 # from flask_cors import CORS
+
+player_list_path = os.path.join(os.path.dirname(__file__), 'player-list.json')
+
 
 app = Flask(__name__)
 # CORS(app)
 
 @app.route('/player-list', methods=["GET"])
+
 # @app.route("/json")
 
 def send_json():
-    with open('player-list.json', 'r') as file:
+    with open(player_list_path, 'r') as file:
         data = json.load(file)
 
     return jsonify(data)
