6da2a92188de03787b17b1a73bb255fb7c48eed0
kermattC <matthew.chan5@uoit.net>
2024-04-28 22:49:27 -0400

    Fixed selected game  z-index

--- client/src/components/players/playersList.jsx
+++ client/src/components/players/playersList.jsx
@@ -3,6 +3,7 @@ import { fetchPlayersList, fetchPlayerStats } from '../../store/utils/thunk';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import Select from 'react-select';
+import { components } from 'react-select';
 
 {/* <Select filterOption={createFilter({ignoreAccents: false})}></Select>  */}
 
@@ -25,7 +26,41 @@ const PlayersList = ({ onPlayerSelect }) => {
         // console.log("Player id: ", option.value.playerID)
         onPlayerSelect(option.value.playerID)
     };
+    
+    // const CustomOption = ({ data, selectProps }) => (
+    //     // <div {...innerProps}>
+    //     <div>
+    //       <span>{data.label}</span>
+    //       <button onClick={(e) => selectProps.onButtonClick(data)}>Button</button>
+    //     </div>
+    // );
+    
+    // const CustomSelect = ({ options, onButtonClick}) => (
+    //     <>
+    //     {/* {console.log("...rest: ", {...rest})} */}
+        
+    //     <Select
+    //     //   {...rest}
+    //       options={options}
+    //       components={{ Option: CustomOption }}
+    //       onButtonClick={onButtonClick}
+    //     />
+    //     </>
+    // );
 
+    const handleClick = (event) => {
+        console.log(event)
+    }
+
+    const CustomButton = ({ children, ...props }) => {
+        return (
+            <>
+                {console.log("Children: ", children)}
+            
+                <button> test</button>
+            </>    
+        )   
+    }
 
     useEffect(() => {
         dispatch(fetchPlayersList())
@@ -47,10 +82,15 @@ const PlayersList = ({ onPlayerSelect }) => {
             {!players.loading ? ( 
                 <>
                 <h5>Select a player</h5>
+                    {/* <CustomSelect
+                        options={playersList}
+                        onButtonClick={(e) => handleClick(e)}
+                    /> */}
                     <Select
                         value={selectedOption}
                         onChange={handleSelectedPlayer}
                         options={playersList}
+                        components={{CustomButton}}
                     />
                 </>
             ) : null}
